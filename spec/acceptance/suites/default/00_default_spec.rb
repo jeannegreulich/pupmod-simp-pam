badd763f5984c285e2052a2d8c53c9dad60ebd6e
jeannegreulich <jeanne.greulich@onyxpoint.com>
2020-02-28 15:06:39 -0500

    (SIMP-7528) Pam authselect
    - skip authconfig disable in EL8
    - update spec_helpers for FIPS
    
    SIMP-7528 #close
    SIMP-7580 #close
    SIMP-7581 #close
    SIMP-7377 #close

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -20,22 +20,32 @@ describe 'pam class' do
           apply_manifest_on(host, manifest, {:catch_changes => true})
         end
 
-        it 'should replace authconfig and authconfi-tui links' do
-          result = on(host, 'ls -l /usr/sbin/authconfig')
-          expect(result.stdout).to match(/authconfig -> \/usr\/local\/sbin\/simp_authconfig.sh/)
+        case  host[:platform]
+        when /el-[67]-x86_64/
+          it 'should replace authconfig and authconfi-tui links' do
+            result = on(host, 'ls -l /usr/sbin/authconfig')
+            expect(result.stdout).to match(/authconfig -> \/usr\/local\/sbin\/simp_authconfig.sh/)
 
-          result = on(host, 'ls -l /usr/sbin/authconfig-tui')
-          expect(result.stdout).to match(/authconfig-tui -> \/usr\/local\/sbin\/simp_authconfig.sh/)
-        end
+            result = on(host, 'ls -l /usr/sbin/authconfig-tui')
+            expect(result.stdout).to match(/authconfig-tui -> \/usr\/local\/sbin\/simp_authconfig.sh/)
+          end
 
-        it 'authconfig and authconfig-tui should have no affect on PAM configuration' do
-          on(host, '/usr/sbin/authconfig --update')
-          # verify Puppet detects no changes to PAM configuration after authconfig is run
-          apply_manifest_on(host, manifest, {:catch_changes => true})
+          it 'authconfig and authconfig-tui should have no affect on PAM configuration' do
+            on(host, '/usr/sbin/authconfig --update')
+            # verify Puppet detects no changes to PAM configuration after authconfig is run
+            apply_manifest_on(host, manifest, {:catch_changes => true})
 
-          # verify Puppet detects no changes to PAM configuration after authconfig-tui is run
-          on(host, '/usr/sbin/authconfig-tui --update')
-          apply_manifest_on(host, manifest, {:catch_changes => true})
+            # verify Puppet detects no changes to PAM configuration after authconfig-tui is run
+            on(host, '/usr/sbin/authconfig-tui --update')
+            apply_manifest_on(host, manifest, {:catch_changes => true})
+          end
+        else
+          it 'should not replace authconfig and authselect should do nothing if not forced' do
+            on(host, 'ls -l /usr/sbin/authconfig', :acceptable_exit_codes => [2] )
+            result = on(host,'/usr/bin/authselect select sssd', :accept_all_exit_codes => true)
+            expect(result.stderr).to match(/Refusing to activate profile/)
+            apply_manifest_on(host, manifest, {:catch_changes => true})
+          end
         end
       end
     end
